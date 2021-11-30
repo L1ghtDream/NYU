47a38e6c4b5688938b2f3543b507d4fcb3d1a4a4
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-30 11:13:01 +0200

    Week 15

--- bmimetric2.cpp
+++ bmimetric2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void bmimetricf(int weight, float height) {
     float bmi = weight / (height * height);
-    cout << setprecision(1) << "BMI is: " << bmi << ", Status is ";
+    cout << setprecision(3) << "BMI is: " << bmi << ", Status is ";
     if (bmi < 18.5) {
         cout << "Underweight";
     } else if (bmi < 24.9) {
