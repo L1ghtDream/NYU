677c03419f645a3bd27481e9806e0db9111bf39e
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-30 10:40:23 +0200

    Week 15

--- bmimetric2.cpp
+++ bmimetric2.cpp
@@ -1,13 +1,15 @@
-float bmimetricf(int weight, float height){
-    float data = weight/(height*height);
-    if(data<18.5){
-        cout<<"Underweight";
-    }else if(data<24.9){
-        cout<<"Normal";
-    }else if(data<29.9){
-        cout<<"Overweight";
-    }else{
-        cout<<"Obese";
+#include "iostream"
+
+float bmimetricf(int weight, float height) {
+    float data = weight / (height * height);
+    if (data < 18.5) {
+        cout << "Underweight";
+    } else if (data < 24.9) {
+        cout << "Normal";
+    } else if (data < 29.9) {
+        cout << "Overweight";
+    } else {
+        cout << "Obese";
     }
     return data;
 }
