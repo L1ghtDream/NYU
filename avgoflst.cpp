3c2791051dce1e5e2c1cc5f09416e2b4f6dda4de
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-30 10:38:56 +0200

    Week 15

--- avgoflst.cpp
+++
@@ -1,7 +0,0 @@
-float avgoflst(int lst[], int size) {
-    int sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += lst[i];
-    }
-    return (sum * 1.0f) / (size * 1.0f);
-}
--- maxinlst.cpp
+++
@@ -1,9 +0,0 @@
-int maxinlst(int lst[], int size) {
-    int maxx = -1000000000;
-    for (int i = 0; i < size; i++) {
-        if(maxx<lst[i]){
-            maxx=lst[i];
-        }
-    }
-    return maxx;
-}
