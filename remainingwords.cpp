a410584b3dc0b4a140889755f0a39ac1c963cdbb
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-30 12:00:55 +0200

    Week 15

--- remainingwords.cpp
+++ remainingwords.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 
 
-string remainingwords(string s){
-    return s.substr(s.find(" "), s.length());
+string remainingwords(string s) {
+    return s.substr(s.find(" ") + 1, s.length() - 1);
 }
