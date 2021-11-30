89c7957d82b713ed5561ba30247f7512d6d1009e
L1ghtDream <radu.voinea.mihai@gmail.com>
2021-11-30 11:09:22 +0200

    Week 15

--- bmimetric2.cpp
+++ bmimetric2.cpp
@@ -1,15 +1,28 @@
 #include "iostream"
 
-float bmimetricf(int weight, float height) {
-    float data = weight / (height * height);
-    if (data < 18.5) {
-        std::cout << "Underweight";
-    } else if (data < 24.9) {
-        std::cout << "Normal";
-    } else if (data < 29.9) {
-        std::cout << "Overweight";
+using namespace std;
+
+void bmimetricf(int weight, float height) {
+    float bmi = weight / (height * height);
+    cout << "BMI is: " << bmi << ", Status is ";
+    if (bmi < 18.5) {
+        cout << "Underweight";
+    } else if (bmi < 24.9) {
+        cout << "Normal";
+    } else if (bmi < 29.9) {
+        cout << "Overweight";
     } else {
-        std::cout << "Obese";
+        cout << "Obese";
     }
-    return data;
+}
+
+int main() {
+    float weight;
+    float height;
+    cout << "Please enter weight in kilograms: ";
+    cin >> weight;
+    cout << "Please enter height in meters: ";
+    cin >> height;
+    bmimetricf(weight, height);
+
 }
