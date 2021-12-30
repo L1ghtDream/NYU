65ad0187c17fde4aecc2e0ba2be0d1389f8d75e1
LightDream <radu.voinea.mihai@gmail.com>
2021-12-30 21:59:30 +0200

    Week 18

--- addone.cpp
+++ addone.cpp
@@ -1,18 +1,6 @@
-#include <iostream>
-using namespace std;
-
 void addOne(int *ptrNum);
 
 void addOne(int *ptrNum){
     (*ptrNum)++;
-}
-
-int main() {
-    int tmp = 10;
-    int *tmpPtr = &tmp;
-
-    addOne(tmpPtr);
-
-    cout<<tmp;
 }
 
