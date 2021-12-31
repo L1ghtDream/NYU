3034a703396cfc0def8dc04342d51cb6a7de16e6
LightDream <radu.voinea.mihai@gmail.com>
2021-12-31 15:44:08 +0200

    Week 26 - 3

---
+++ showticket.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class ShowTicket;
+
+class ShowTicket {
+private:
+    bool sold;
+
+protected:
+    string row;
+    string seat;
+
+public:
+    ShowTicket(string row, string seat) {
+        this->row = row;
+        this->seat = seat;
+        this->sold = false;
+    }
+
+    bool is_sold();
+
+    void sell_seat();
+
+    string print_ticket();
+
+};
+
+bool ShowTicket::is_sold() {
+    return sold;
+}
+
+void ShowTicket::sell_seat() {
+    sold = true;
+}
+
+string ShowTicket::print_ticket() {
+    return row + " " + seat + " " + (is_sold() ? "sold" : "available");
+}
--- sportticket.h
+++ sportticket.h
@@ -33,5 +33,7 @@ void SportTicket::sell_beer() {
 }
 
 string SportTicket::print_ticket() {
-    return ShowTicket::print_ticket() + " " + (beer_sold() ? "bear" : "nobear");
+    //AA 101 sold nobeer;
+    return row + " " + seat + " " + (SportTicket::is_sold() ? "sold" : "available") + + " " + (beer_sold() ? "bear" : "nobear");;
+    //ShowTicket::print_ticket() + " " + (beer_sold() ? "bear" : "nobear");
 }
