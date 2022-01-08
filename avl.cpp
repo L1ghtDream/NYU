933130c616a57874ab92826e3427f45c7d9f6657
LightDream <radu.voinea.mihai@gmail.com>
2022-01-08 21:32:15 +0200

    Week 35

--- avl.cpp
+++ avl.cpp
@@ -55,19 +55,39 @@ Node *insertnb(Node *node, int key) {
 }
 
 Node *left_rotate(Node *x) {
-    Node *tmp;
-    tmp = x->right;
-    x->right = tmp->left;
-    tmp->left = x;
-    return tmp;
+    Node *y = x->right;
+    Node *T2 = y->left;
+
+    // Perform rotation
+    y->left = x;
+    x->right = T2;
+
+    // Update heights
+    x->height = max(height(x->left),
+                    height(x->right)) + 1;
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+
+    // Return new root
+    return y;
 }
 
-Node *right_rotate(Node *x) {
-    Node *tmp;
-    tmp = x->left;
-    x->left = tmp->right;
-    tmp->right = x;
-    return tmp;
+Node *right_rotate(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    // Perform rotation
+    x->right = y;
+    y->left = T2;
+
+    // Update heights
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+    x->height = max(height(x->left),
+                    height(x->right)) + 1;
+
+    // Return new root
+    return x;
 }
 
 int get_balance(Node *x) {
