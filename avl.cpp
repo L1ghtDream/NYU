8fbf88b4dd5933dfdab077ad496b509ca371303d
LightDream <radu.voinea.mihai@gmail.com>
2022-01-08 21:17:35 +0200

    Week 35

--- avl.cpp
+++ avl.cpp
@@ -11,12 +11,16 @@ Node *left_rotate(Node *x);
 
 Node *right_rotate(Node *x);
 
+int get_balance(Node *x);
+
+int height(Node *N);
+
 Node *new_node(int key) {
     Node *node = new Node;
     node->key = key;
     node->height = 1;
-    node->left= nullptr;
-    node->right= nullptr;
+    node->left = nullptr;
+    node->right = nullptr;
     return node;
 }
 
@@ -56,12 +60,26 @@ Node *left_rotate(Node *x) {
     return tmp;
 }
 
-Node *right_rotate(Node *x){
+Node *right_rotate(Node *x) {
     Node *tmp;
     tmp = x->left;
     x->left = tmp->right;
     tmp->right = x;
     return tmp;
+}
+
+int get_balance(Node *x) {
+    if (x == nullptr){
+        return 0;
+    }
+    return height(x->left) - height(x->right);
+}
+
+int height(Node *N) {
+    if (N == nullptr){
+        return 0;
+    }
+    return N->height;
 }
 /*
 
