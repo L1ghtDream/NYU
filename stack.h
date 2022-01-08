0b9208fba62f8561eaab5119389d86b0cbb360c8
LightDream <radu.voinea.mihai@gmail.com>
2022-01-08 14:55:16 +0200

    Week-33

---
+++ stack.h
@@ -0,0 +1,33 @@
+#define MAX 1000
+
+class Stack {
+public:
+    int top;
+    int a[MAX]; // Maximum size of Stack
+    Stack() { top = -1; }
+
+    bool push(int x);
+
+    int pop();
+
+    int peek();
+
+    bool isEmpty();
+};
+
+bool Stack::push(int x) {
+    top++;
+    a[top]=x;
+}
+
+int Stack::pop() {
+    return a[top--];
+}
+
+int Stack::peek() {
+    
+}
+
+bool Stack::isEmpty() {
+    return top==-1;
+}
