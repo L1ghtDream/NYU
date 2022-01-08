009f0eb2d127014d73b5d7e7039b90ba7d243cb3
LightDream <radu.voinea.mihai@gmail.com>
2022-01-08 19:09:14 +0200

    Week 35

--- avl.cpp
+++ avl.cpp
@@ -2,9 +2,25 @@
 
 Node *new_node(int key);
 
+Node *insertnb(Node *node, int key);
+
 Node *new_node(int key) {
     Node *node = new Node;
     node->key = key;
     node->height = 1;
+    return node;
+}
+
+Node *insertnb(Node *node, int key) {
+    if (node == nullptr) {
+        return new_node(key);
+    }
+
+    if (node->key < key) {
+        node->right = new_node(key);
+    } else {
+        node->left = new_node(key);
+    }
+
     return node;
 }
