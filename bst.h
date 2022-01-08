ce1fd8826aca5ccefcd5b6f46db75b7ffebb50c8
LightDream <radu.voinea.mihai@gmail.com>
2022-01-08 15:48:54 +0200

    Week 35

--- bst.h
+++ bst.h
@@ -13,11 +13,15 @@ public:
 };
 
 BST::BST() {
-    data = 0;
+    this->data = 0;
+    this->left = nullptr;
+    this->right = nullptr;
 }
 
 BST::BST(int data) {
     this->data = data;
+    this->left = nullptr;
+    this->right = nullptr;
 }
 
 void BST::insert(int data) {
